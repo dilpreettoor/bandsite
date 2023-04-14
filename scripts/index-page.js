2cf494a13aacbde08be4e70c5a6940c2f65c03b5
dilpreettoor <dilpreet.s.t@gmail.com>
2023-04-14 18:08:56 -0400

    added javascript

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,12 +7,12 @@ let comments = [
     {
       name: "Emilie Beach",
       timestamp: "01/09/2021",
-      text: "This is art."
+      text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
       name: "Miles Acosta",
       timestamp: "12/20/2020",
-      text: "This is art."
+      text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
   ];
 
@@ -21,14 +21,26 @@ let comments = [
     let name = document.createElement("h3");
     let timestamp = document.createElement("p");
     let commentText = document.createElement("p");
-
+  
     name.textContent = comment.name;
     timestamp.textContent = comment.timestamp;
-    commentText.textContent = comment.commentText;
+    commentText.textContent = comment.text;
+  
+    commentDiv.appendChild(name);
+    commentDiv.appendChild(timestamp);
+    commentDiv.appendChild(commentText);
+  
+    commentDiv.style.display = "flex";
+    commentDiv.style.flexDirection = "column";
+    commentDiv.style.alignItems = "flex-start";
+    commentDiv.style.width = "100%";
 
-    commentContainer.appendChild(name);
-    commentContainer.appendChild(timestamp);
-    commentContainer.appendChild(commentText);
-
-    document.getElementById("comment").appendChild(commentDiv);
+    
+  
+    document.getElementById("comment-section").appendChild(commentDiv);
+  }
+  
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
+    showComments(comment);
   }

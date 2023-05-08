ff0af18d25360307e5de0660560162a320c5d597
dilpreettoor <dilpreet.s.t@gmail.com>
2023-05-07 22:21:54 -0400

    added dummy image to all the comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,34 +1,45 @@
 
 
-  const showComments = (comment) => {
-    let commentDiv = document.createElement("div");
-    let commentTop = document.createElement("div");
-    let name = document.createElement("h3");
-    let timestamp = document.createElement("p");
-    let commentText = document.createElement("p");
-  
-    name.textContent = comment.name;
-    const dateObj = new Date(comment.timestamp);
-    const formattedDated = dateObj.toLocaleDateString();
-    timestamp.textContent = formattedDated;
-    commentText.textContent = comment.comment;
-    commentTop.appendChild(name);
-    commentTop.appendChild(timestamp);
-    commentDiv.appendChild(commentTop);
-    commentDiv.appendChild(commentText);
-    commentDiv.style.display = "flex";
-    commentDiv.style.flexDirection = "column";
-    commentDiv.style.alignItems = "flex-start";
-    commentDiv.style.width = "100%";
-    commentTop.style.display = "flex";
-    commentTop.style.flexDirection = "row";
-    commentTop.style.justifyContent = "space-between";
-    commentTop.style.width = "100%";
-    commentText.style.width = "100%";
-    name.style.margin = "0";
-    timestamp.style.marginTop = "1.4rem";
-    document.getElementById("comment-section").appendChild(commentDiv);
-  };
+const showComments = (comment) => {
+  let commentDiv = document.createElement("div");
+  let commentTop = document.createElement("div");
+  let nameDiv = document.createElement("div");
+  nameDiv.classList.add("div-image");
+  let image = document.createElement("img");
+  let name = document.createElement("h3");
+  let timestamp = document.createElement("p");
+  let commentText = document.createElement("p");
+
+  image.src = "https://dummyimage.com/50x50";
+  image.alt = "Dummy Image";
+  image.style.marginRight = "1rem";
+  image.style.width = "50px";
+  image.style.height = "50px";
+  name.textContent = comment.name;
+  const dateObj = new Date(comment.timestamp);
+  const formattedDated = dateObj.toLocaleDateString();
+  timestamp.textContent = formattedDated;
+  commentText.textContent = comment.comment;
+  nameDiv.appendChild(image);
+  nameDiv.appendChild(name);
+  commentTop.appendChild(nameDiv);
+  commentTop.appendChild(timestamp);
+  commentDiv.appendChild(commentTop);
+  commentDiv.appendChild(commentText);
+  commentDiv.style.display = "flex";
+  commentDiv.style.flexDirection = "column";
+  commentDiv.style.alignItems = "flex-start";
+  commentDiv.style.width = "100%";
+  commentTop.style.display = "flex";
+  commentTop.style.flexDirection = "row";
+  commentTop.style.justifyContent = "space-between";
+  commentTop.style.width = "100%";
+  commentText.style.width = "100%";
+  name.style.margin = "0";
+  timestamp.style.marginTop = "1.4rem";
+  document.getElementById("comment-section").appendChild(commentDiv);
+};
+
   
   
 
